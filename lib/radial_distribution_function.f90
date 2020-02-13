cf0cc6d53cbd9a942a7e4bdc964478c3c46c003f
thielf <thiago@DESKTOP-I2LCFHC.localdomain>
2020-02-13 00:50:03 -0200

    ...

--- lib/radial_distribution_function.f90
+++ lib/radial_distribution_function.f90
@@ -15,14 +15,14 @@ subroutine radial_distribution_one_particle_species(N,filename,snaps,LL,avg,r)
    DOUBLE PRECISION::rr,delg,pi,xr,yr,zr,r2,vb,nid,rho
    DOUBLE PRECISION,DIMENSION(10000, Nhis)::gr
         
-    delg=LL/(2.*Nhis)
-    pi=4*ATAN(1.)
-    N1 = N
-    rho = N1/(LL**3.0d0)
-    gr = 0.0d0
-    avg(:)=0.d0
+   delg=LL/(2.*Nhis)
+   pi=4*ATAN(1.)
+   N1 = N
+   rho = N1/(LL**3.0d0)
+   gr = 0.0d0
+   avg(:)=0.d0
 
-    open(10, file=filename)
+   open(10, file=filename)
 
    do j = 1, snaps 
       read(10,*) dumb1 
@@ -33,55 +33,55 @@ subroutine radial_distribution_one_particle_species(N,filename,snaps,LL,avg,r)
       enddo 
    enddo 
 
-DO k=1,snaps
-   DO i=1,N-1
-      DO j=i+1,N
-         xr=x(k,i)-x(k,j)
-         yr=y(k,i)-y(k,j)
-         zr=z(k,i)-z(k,j)
-            
-         xr=xr-LL*(NINT(xr/LL))
-         yr=yr-LL*(NINT(yr/LL))
-         zr=zr-LL*(NINT(zr/LL))
-         r2=xr*xr+yr*yr+zr*zr
-         rr=SQRT(r2)
+   DO k=1,snaps
+      DO i=1,N-1
+         DO j=i+1,N
+            xr=x(k,i)-x(k,j)
+            yr=y(k,i)-y(k,j)
+            zr=z(k,i)-z(k,j)
+               
+            xr=xr-LL*(NINT(xr/LL))
+            yr=yr-LL*(NINT(yr/LL))
+            zr=zr-LL*(NINT(zr/LL))
+            r2=xr*xr+yr*yr+zr*zr
+            rr=SQRT(r2)
 
-         IF(rr.LT.LL/2.d0)THEN
-            ig=ceiling(rr/delg)
-            gr(k,ig)=gr(k,ig)+2.
-         END IF
+            IF(rr.LT.LL/2.d0)THEN
+               ig=ceiling(rr/delg)
+               gr(k,ig)=gr(k,ig)+2.
+            END IF
+         END DO
       END DO
    END DO
-END DO
 
 
 
 
-DO j=1,Nhis
-   DO i=1,snaps
-      r(j)=delg*(j+0.5)
-      vb=((j+1)**3.-j**3.)*delg**3.
-      nid=(4./3.)*pi*vb*rho
-      gr(i,j)=gr(i,j)/(N1*nid)
+   DO j=1,Nhis
+      DO i=1,snaps
+         r(j)=delg*(j+0.5)
+         vb=((j+1)**3.-j**3.)*delg**3.
+         nid=(4./3.)*pi*vb*rho
+         gr(i,j)=gr(i,j)/(N1*nid)
+      END DO
    END DO
-END DO
 
 
 
 
 
-DO i=1,Nhis
-   DO j=1,snaps
-      avg(i)=avg(i)+gr(j,i)
+   DO i=1,Nhis
+      DO j=1,snaps
+         avg(i)=avg(i)+gr(j,i)
+      END DO
    END DO
-END DO
 
-OPEN(unit=2,file='teste_read_xyz.dat',action="write")
+   OPEN(unit=2,file='rdf_one_species.dat',action="write")
 
-DO i=1,Nhis
-   WRITE(2,'(2(f17.10,1X))')r(i),avg(i)/snaps
-   avg(i) = avg(i)/snaps
-END DO
+   DO i=1,Nhis
+      WRITE(2,'(2(f17.10,1X))')r(i),avg(i)/snaps
+      avg(i) = avg(i)/snaps
+   END DO
 
 end subroutine 
 
