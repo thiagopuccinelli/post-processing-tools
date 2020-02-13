9ab0f56b9916ccb3a3cdc0367d71b078ce0be6f4
thielf <thiago@DESKTOP-I2LCFHC.localdomain>
2020-02-13 00:45:15 -0200

    ...

--- code.py
+++ code.py
@@ -1,27 +1,27 @@
-import radial_distribution_function 
+import radial_distribution_evaluation 
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-#print(radial_distribution.__doc__)
+print(radial_distribution_evaluation.__doc__)
 
 
-avg,r = radial_distribution.radial_distribution_one_particle_species(1000,"dump.simulation",200,12.59921049894873)
+#avg,r = radial_distribution_evaluation.radial_distribution_one_particle_species(1000,"dump.simulation",200,12.59921049894873)
 
 
 
 # # LATEX:
-plt.rc('text', usetex=True)
+# plt.rc('text', usetex=True)
 
-fig,ax = plt.subplots()
+# fig,ax = plt.subplots()
 
-ax.plot(r,avg,ls="-",color="black")
-ax.set_ylabel(r'$g(r)$',fontsize=28)
-ax.set_xlabel(r'$r$',fontsize=28)
-ax.set_xlim(0,3.5)
-ax.set_ylim(0,4.0)
-plt.yticks(np.arange(0.,4.1, step=0.5),fontsize=22)
-plt.xticks(np.arange(0., 3.51, step=0.5),fontsize=22)
-ax.grid()
-plt.tight_layout()
+# ax.plot(r,avg,ls="-",color="black")
+# ax.set_ylabel(r'$g(r)$',fontsize=28)
+# ax.set_xlabel(r'$r$',fontsize=28)
+# ax.set_xlim(0,3.5)
+# ax.set_ylim(0,4.0)
+# plt.yticks(np.arange(0.,4.1, step=0.5),fontsize=22)
+# plt.xticks(np.arange(0., 3.51, step=0.5),fontsize=22)
+# ax.grid()
+# plt.tight_layout()
 
-plt.show()
+# plt.show()
