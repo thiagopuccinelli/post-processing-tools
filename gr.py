b79add260a84f08de84f053375458edc1c84dd76
thielf <thiago@DESKTOP-IN6L9OI.localdomain>
2021-09-27 16:17:48 -0300

    excess entropy calculation subroutine added to lib

--- gr.py
+++ gr.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import math 
 import matplotlib.pyplot as plt 
-from tools import rdf_monomeric2d, rdf_monomer_to_monomer
+from tools import excess_entropy_from_rdf,rdf_monomeric2d, rdf_monomer_to_monomer
 
 ##### read in lammps snap
 N = int(np.genfromtxt('../core-softened-phase-diagram/CSA/temp-0.01/lambda_0.60/run/lammps_temp_0.01_press_8.5.snap', max_rows=1))
@@ -9,7 +9,7 @@ traj = np.genfromtxt(
     '../core-softened-phase-diagram/CSA/temp-0.01/lambda_0.60/run/lammps_temp_0.01_press_8.5.snap', skip_header=2,
     invalid_raise=False)[:, 1:4].reshape(-1, N, 3)
 
-print(rdf_monomer_to_monomer.__doc__)
+#print(rdf_monomer_to_monomer.__doc__)
 
 snaps = len(traj[:,:,:])
 L1 = 1.1*np.absolute(np.max(traj[0,:,0]) - np.min(traj[0,:,0])) 
@@ -18,11 +18,21 @@ L3 = 0.0
 print(np.shape(traj[:,:,0]))
 rdf, r = rdf_monomer_to_monomer(traj[:100,:,0],traj[:100,:,1],L1,L2)
 
+rho = (0.5 * N) / (L1 * L2)
+
+print(excess_entropy_from_rdf.__doc__)
+
+sex, csum = excess_entropy_from_rdf(rdf,r)
+
+print(r)
+#csum = csum[csum>0]
+#print(csum)
+
 plt.figure()
-plt.ylim(0,12)
-data = np.genfromtxt("../core-softened-phase-diagram/CSA/temp-0.01/lambda_0.60/gr/rdf_lammps_temp_0.01_press_8.5_mon_mon.dat")
-plt.plot(r,rdf)
-plt.plot(data[:,0],data[:,1],color="red")
+#plt.ylim(0,12)
+data = np.genfromtxt("../core-softened-phase-diagram/CSA/temp-0.01/lambda_0.60/ex_entropy/press_8.5_cumsex.dat")
+plt.loglog(r,csum)
+plt.loglog(data[:,0],data[:,1],color="red")
 plt.show()
 
 #np.savetxt("teste.dat", np.column_stack([r,rdf]))
