corrected rdf for rmax, introduced cs2 computation

python/post_processing.py
@@ -13,8 +13,8 @@ class post_processing:
        trajectory = tools.read_in_trajectory(filename,Npart,snaps)
        return trajectory 
 
-    def rdf_compute2d(rx,ry,Lx,Ly,nbins):
-        return tools.rdf_monomeric2d(rx,ry,Lx,Ly,nbins)
+    def rdf_compute2d(rx,ry,Lx,Ly,nbins,rmax):
+        return tools.rdf_monomeric2d(rx,ry,Lx,Ly,nbins,rmax)
 
     def compute_s2_from_rdf3d(rdf,r,Npart,Lx,Ly,Lz):
         sex = 0.0 
@@ -34,3 +34,6 @@ class post_processing:
         sex =  np.pi * rho * sex 
         return sex  
 
+    def excess_entropy_cumulant_from_rdf(rdf,r,nbins):
+        cs2, rcs2 = tools.excess_entropy_cumulant_from_rdf(rdf,r,nbins)
+        return cs2[cs2>0],rcs2[rcs2>0]