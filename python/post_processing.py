07542e11f531eea32010eb2ef30645097a69cfe8
thiagopuccinelli <thiagoponogueira@gmail.com>
2023-08-17 19:10:55 +0200

    creating python module to acsess fortran comps

--- python/post_processing.py
+++ python/post_processing.py
@@ -6,6 +6,7 @@ class post_processing:
     def __init__(self):
         self.read_trajectory()
         self.rdf_compute()
+        self.compute_s2_from_rdf()
 
     def read_trajectory(filename,Npart,snaps):
        trajectory = tools.read_in_trajectory(filename,Npart,snaps)
@@ -13,3 +14,22 @@ class post_processing:
 
     def rdf_compute(rx,ry,Lx,Ly,nbins):
         return tools.rdf_monomeric2d(rx,ry,Lx,Ly,nbins)
+
+    def compute_s2_from_rdf3d(rdf,r,Npart,Lx,Ly,Lz):
+        sex = 0.0 
+        for i,val in enumerate(rdf):
+            if val > 0:
+                sex += (rdf[i] * np.log(rdf[i]) - (rdf[i] - 1))*r[i]**2.
+        rho = ( Npart / (Lx * Ly * Lz ))
+        sex = 2. * np.pi * rho * sex 
+        return sex  
+    
+    def compute_s2_from_rdf2d(rdf,r,Npart,Lx,Ly,Lz):
+        sex = 0.0 
+        for i,val in enumerate(rdf):
+            if val > 0:
+                sex += (rdf[i] * np.log(rdf[i]) - (rdf[i] - 1))*r[i]
+        rho = ( Npart / (Lx * Ly))
+        sex =  np.pi * rho * sex 
+        return sex  
+
--- python/test.py
+++ python/test.py
@@ -10,6 +10,8 @@ Lx = 1.1*np.absolute(np.max(data[0,:,0]) - np.min(data[0,:,0]))
 Ly = 1.1*np.absolute(np.max(data[0,:,1]) - np.min(data[0,:,1]))
 rdf,r = post_processing.rdf_compute(data[:,:,0],data[:,:,1],Lx,Ly,256)
 
+print(post_processing.compute_s2_from_rdf2d(rdf,r,200,Lx,Ly,1.0))
+
 plt.figure()
 
 plt.plot(r,rdf)
